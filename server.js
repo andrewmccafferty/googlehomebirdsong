17d89eedc6c3491842e532aadec91eb852417f52
Andrew McCafferty <andrew.mccafferty@theappbusiness.com>
2018-04-29 15:55:50 +0100

    Further fixes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {actionssdk} = require('actions-on-google');
-const request = require('request');
+const requestPromise = require('request-promise');
 const app = actionssdk({debug: true});
 const port = process.env.PORT || 5000;
 // fulfillment code here
@@ -11,12 +11,16 @@ app.intent('actions.intent.MAIN', (conv) => {
     'Say a bird species and I\'ll play it for you.</speak>');
 });
 
+const getRandomArrayElement = (items) => {
+  return items[Math.floor(Math.random()*items.length)];
+}
+
 app.intent('actions.intent.TEXT', (conv, input) => {
   if (input === 'bye') {
     return conv.close('Goodbye!');
   }
   let url = `https://www.xeno-canto.org/api/2/recordings?query=${encodeURIComponent(input)}`;
-  return request(url, function(error, response, body) {
+  return requestPromise(url).then((body) => {
     let parsedBody = JSON.parse(body);
       console.log('Got response back from api');
         let recordings = parsedBody.recordings;
@@ -28,10 +32,10 @@ app.intent('actions.intent.TEXT', (conv, input) => {
             return conv.close('Goodbye');
         }
       
-        let randomRecording = arrayHelpers.getRandomArrayElement(recordings);
+        let randomRecording = getRandomArrayElement(recordings);
         let recordingUrl = `https://www.xeno-canto.org/${randomRecording.id}/download`;
         console.log(`Playing recording {${randomRecording.id}}`);
-        conv.ask(`<speak>Here is a ${input} for you <audio src=${recordingUrl}></audio></speak>`);
+        conv.ask(`<speak>Here is a ${input} for you <audio src="${recordingUrl}"></audio></speak>`);
   });
 });
 
